75b0983d7d1892c255ea1c4398f56b8ebe61861d
Rizky Putra <140175185+rizkyputraa04@users.noreply.github.com>
2026-03-28 13:03:47 +0700

    Add files via upload

---
+++ WebApp/scr/App.tsx
@@ -0,0 +1,34 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Navbar from "@/components/Navbar";
+import Index from "./pages/Index.tsx";
+import Agents from "./pages/Agents.tsx";
+import Jobs from "./pages/Jobs.tsx";
+import Hire from "./pages/Hire.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/hire" element={<Hire />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
